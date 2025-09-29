418f23e2fc51a239e54e43f434474be0640ae109
David <david.tobola2003@gmail.com>
2025-09-29 20:06:21 +0200

    Delete libcache.h

--- libcache.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBCACHE_H
-#define LIBCACHE_H
-
-void cache_access(const char *path);
-void cache_stat(const char *path);
-void cache_lstat(const char *path);
-void cache_fstat(const char *path);
-
-#endif
